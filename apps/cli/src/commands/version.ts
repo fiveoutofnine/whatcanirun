0742ccca85c16349995a2be1f3b98a9e37c63400
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-17 13:51:12 -0700

    chore: update version

--- apps/cli/src/commands/version.ts
+++ apps/cli/src/commands/version.ts
@@ -1,13 +1,18 @@
 import { defineCommand } from 'citty';
 
+import * as log from '../utils/log';
+
 const command = defineCommand({
   meta: {
     name: 'version',
     description: 'Print version information',
   },
   run() {
-    console.log('whatcanirun 0.1.0');
-    console.log('harness schema v1');
+    log.blank();
+    log.header('whatcanirun');
+    log.label('Version', '0.1.0');
+    log.label('Schema', 'v1');
+    log.blank();
   },
 });
 
