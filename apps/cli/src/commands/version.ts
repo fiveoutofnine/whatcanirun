b6e1a0df2a59026cc871ca393c6a1b5d7e7915da
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-19 20:00:35 -0700

    chore: bump versino

--- apps/cli/src/commands/version.ts
+++ apps/cli/src/commands/version.ts
@@ -10,8 +10,7 @@ const command = defineCommand({
   run() {
     log.blank();
     log.header('whatcanirun');
-    log.label('Version', '0.1.3');
-    log.label('Schema', 'v1');
+    log.label('Version', '0.1.4');
     log.blank();
   },
 });
