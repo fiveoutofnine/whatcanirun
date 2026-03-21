8242337a7f1131fbc0fc4ea7897fc09a2bbb1d03
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-21 15:27:56 -0700

    chore: redo version command logs

--- apps/cli/src/commands/version.ts
+++ apps/cli/src/commands/version.ts
@@ -2,18 +2,13 @@ import { HARNESS_VERSION } from '@whatcanirun/shared';
 import chalk from 'chalk';
 import { defineCommand } from 'citty';
 
-import * as log from '../utils/log';
-
 const command = defineCommand({
   meta: {
     name: 'version',
     description: 'Print version information',
   },
   run() {
-    console.log();
-    console.log(chalk.bold('whatcanirun'));
-    log.label('Version', HARNESS_VERSION);
-    console.log();
+    console.log(`${chalk.bold(HARNESS_VERSION)} (whatcanirun)`);
   },
 });
 
