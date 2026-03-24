558bed641c6b98db9e929f4f3c752e99760a21ed
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-24 10:55:30 -0700

    feat: Ini hero

---
+++ apps/www/app/(app)/(components)/hero.tsx
@@ -0,0 +1,26 @@
+import Logo from '@/components/common/logo';
+import { Button } from '@/components/ui';
+
+// -----------------------------------------------------------------------------
+// Component
+// -----------------------------------------------------------------------------
+
+const Hero: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-3xl font-bold leading-snug tracking-tight text-gray-12 md:text-5xl md:leading-snug">
+        <Logo className="inline select-text text-3xl md:text-5xl" />?
+      </h1>
+      <div className="flex gap-2">
+        <Button variant="outline" href="/docs">
+          bunx whatcanirun@latest
+        </Button>
+        <Button variant="ghost" href="/docs">
+          Docs
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -1,22 +1,11 @@
-import { db } from '@/lib/db';
+import Hero from './(components)/hero';
 
 import ContainerLayout from '@/components/layouts/container';
 
 export default async function Page() {
-  /* const data = await db.query.runs.findMany({
-    columns: {
-      id: true,
-    },
-    with: {
-      user: true,
-      model: true,
-    }
-  }); */
-  const data = await db.query.runs.findMany({});
-
   return (
     <ContainerLayout className="flex flex-col space-y-4">
-      <pre className="whitespace-break w-full">{JSON.stringify(data, null, 2)}</pre>
+      <Hero />
     </ContainerLayout>
   );
 }
