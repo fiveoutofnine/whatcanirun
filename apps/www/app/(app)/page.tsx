70534fa88996a7fe0628e1f456b9c46c4f7c3cb6
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-03-24 09:58:53 -0700

    chore: update

--- apps/www/app/(app)/page.tsx
+++ apps/www/app/(app)/page.tsx
@@ -1,5 +1,19 @@
 import ContainerLayout from '@/components/layouts/container';
+import { db } from '@/lib/db';
 
-export default function Page() {
-  return <ContainerLayout className="flex flex-col space-y-4">whatcani.run</ContainerLayout>;
+export default async function Page() {
+  /* const data = await db.query.runs.findMany({
+    columns: {
+      id: true,
+    },
+    with: {
+      user: true,
+      model: true,
+    }
+  }); */
+  const data = await db.query.runs.findMany({});
+
+  return <ContainerLayout className="flex flex-col space-y-4">
+    <pre className='whitespace-break w-full'>{JSON.stringify(data, null, 2)}</pre>
+  </ContainerLayout>;
 }
