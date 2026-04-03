07597efc3e57aa955c0e1ffc093de906803eda19
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-04-02 22:03:29 -0700

    chore: update

--- apps/www/app/(app)/[orgSlug]/[modelSlug]/(components)/device-floating-selector.tsx
+++ apps/www/app/(app)/[orgSlug]/[modelSlug]/(components)/device-floating-selector.tsx
@@ -96,8 +96,8 @@ const DeviceFloatingSelector: React.FC<DeviceFloatingSelectorProps> = ({ chips }
     <div className="pointer-events-none fixed inset-x-0 bottom-4 z-50 flex justify-center">
       <DeviceCombobox devices={chipsSorted} value={device} onSelect={setDevice}>
         <button className="pointer-events-auto h-8 rounded-full border border-gray-7 bg-gray-3 shadow-lg backdrop-blur transition-colors hover:border-gray-8 hover:bg-gray-4 focus-visible:border-gray-8 focus-visible:bg-gray-4 active:bg-gray-5">
-          <span className="pr-32text-base flex items-center gap-1.5 pl-1.5 pr-2.5">
-            {Logo ? <Logo size={20} className="rounded-full border border-gray-6" /> : null}
+          <span className="pr-32text-base flex items-center gap-1.5 pl-1 pr-2.5">
+            {Logo ? <Logo size={24} className="rounded-full border border-gray-6" /> : null}
             <span className="font-medium text-gray-12">{buttonContent}</span>
             <ChevronsUpDown className="size-4 text-gray-11" />
           </span>
