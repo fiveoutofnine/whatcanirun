926f570bed95a64b1777d314d5085d84dfbe6582
e6f4e37l <66016924+fiveoutofnine@users.noreply.github.com>
2026-04-09 17:18:06 -0700

    chore: update metadata

--- apps/www/app/(app)/run/[id]/layout.tsx
+++ apps/www/app/(app)/run/[id]/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next';
+import { unstable_cache as cache } from 'next/cache';
 import { Fragment } from 'react';
+
+import { db } from '@/lib/db';
+import { parseManufacturer } from '@/lib/utils';
 
 // -----------------------------------------------------------------------------
 // Metadata
@@ -12,12 +16,90 @@ export const generateMetadata = async ({
 }): Promise<Metadata> => {
   const { id } = await params;
 
+  const run = await cache(
+    async () =>
+      db.query.runs.findFirst({
+        columns: {
+          modelId: true,
+          deviceId: true,
+        },
+        with: {
+          model: {
+            columns: {
+              displayName: true,
+              quant: true,
+            },
+            with: {
+              info: {
+                columns: {
+                  name: true,
+                  quant: true,
+                },
+              },
+            },
+          },
+          device: {
+            columns: {
+              gpu: true,
+              cpu: true,
+              cpuCores: true,
+            },
+          },
+        },
+        where: (run, { eq }) => eq(run.id, id),
+      }),
+    [`run-${id}`],
+    { revalidate: 600 },
+  )();
+
+  if (!run) return {};
+
+  const modelName = run.model?.info?.name ?? run.model.displayName;
+  const quant = run.model?.info?.quant ?? run.model.quant;
+  const { manufacturer, displayName } = parseManufacturer(run.device.gpu);
+  const isApple = manufacturer === 'apple';
+  const article = isApple || 'aeiou'.includes(displayName.charAt(0).toLowerCase()) ? 'an' : 'a';
+
   const title = `Run Details`;
-  const description = `View details for run ${id}.`;
+  const description =
+    `View details for a benchmark run of ${modelName} ${quant ? `(${quant} quant) ` : ''}` +
+    displayName
+      ? `on ${article} ${displayName}.`
+      : '.';
+  const url = `https://whatcani.run/run/${id}`;
+  const images = [
+    {
+      url: 'https://whatcani.run/images/og/home.png',
+      alt: 'whatcani.run OpenGraph image',
+      width: 1200,
+      height: 630,
+    },
+  ];
 
   return {
     title,
     description,
+    openGraph: {
+      title,
+      description,
+      url,
+      siteName: 'whatcani.run',
+      locale: 'en_US',
+      type: 'website',
+      images,
+    },
+    twitter: {
+      title,
+      description,
+      card: 'summary_large_image',
+      creator: '@fiveoutofnine',
+      creatorId: '1269561030272643076',
+      images,
+    },
+    alternates: {
+      canonical: url,
+    },
+    manifest: '/manifest.json',
   };
 };
 
